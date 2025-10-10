c8f021b3cc056cd1a0fe82a115bdb690c6d8dc47
obrauten <obrauten@gmail.com>
2025-10-10 12:35:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -140,7 +140,7 @@ with left:
     ax2.set_ylabel("kWh/m²", fontsize=11, color=PRIMARY)
     ax2.set_ylim(0, max(vals)*1.25)
     ax2.set_title("Gjennomsnittlig årlig energibruk pr kvadratmeter\noppvarmet areal",
-                  fontsize=18, color=Secondary, fontweight="bold", pad=10
+                  fontsize=18, color=Secondary, fontweight="bold", pad=10)
     for t in ax2.get_xticklabels():
         t.set_rotation(20); t.set_ha("right")
     # verdietiketter
